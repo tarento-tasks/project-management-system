b223665107aaa51a6d549caaa99bfb9db3370878
avena-sara <avenasara12@gmail.com>
2025-03-10 14:54:15 +0530

    Pulled JWT-related security files from feature-pooja

---
+++ src/main/java/com/example/project_management_backend/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.project_management_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
+    public SecurityConfig(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) 
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
+                .authorizeHttpRequests(auth -> auth
+                     
+                        .requestMatchers("/api/auth/**").permitAll() 
+                   
+                        .requestMatchers("/api/roles/**").hasRole("ADMIN") 
+                  
+                        .requestMatchers("/api/users/**").hasRole("ADMIN") 
+                        
+                        .requestMatchers("/api/projects/**").hasAnyRole("ADMIN", "MENTOR") 
+                        .requestMatchers("/api/tasks/**").hasAnyRole("ADMIN", "MENTOR") 
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class); 
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
