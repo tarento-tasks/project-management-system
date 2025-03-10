c3170180f9d94fe93a1b1c73f2ef91d51818286c
pooja682002 <pooja.azhi@gmail.com>
2025-03-10 14:16:50 +0530

    Pulled Skill.java from feature-avena

---
+++ src/main/java/com/example/project_management_backend/Model/Skill.java
@@ -0,0 +1,32 @@
+
+package com.example.project_management_backend.Model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+import java.util.UUID;
+
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+@Entity
+@Table(name = "skills")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Skill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // Auto-generate UUID
+    @JdbcTypeCode(SqlTypes.UUID)
+    @Column(name = "skill_id",columnDefinition = "UUID",  nullable = false)
+    private UUID skillId;
+
+    @Column(name = "skill_name", nullable = false, unique = true)
+    private String skillName;
+
+    @OneToMany(mappedBy = "skill", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<SkillMapping> skillMappings;
+}
