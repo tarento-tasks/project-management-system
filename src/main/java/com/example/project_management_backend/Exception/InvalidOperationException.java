7216613dd378bf24038c9c3ed136ebbb8b3ee5f9
avena-sara <avenasara12@gmail.com>
2025-03-17 12:11:08 +0530

    Updated exception handling

---
+++ src/main/java/com/example/project_management_backend/Exception/InvalidOperationException.java
@@ -0,0 +1,12 @@
+package com.example.project_management_backend.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class InvalidOperationException extends RuntimeException {
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+}
+
